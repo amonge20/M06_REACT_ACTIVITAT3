c7a7b8b13f74e865ae4038471f90a258ef6e1e09
amonge20 <2023_aitor.monge@iticbcn.cat>
2024-04-10 20:44:08 +0200

    M06 REACT Gestor de tareas v1.6

---
+++ Practica3/src/components/Formulari.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Formulari = () => {
+    const [tasques, setTasques] = useState([]);
+        
+    return(
+        <div>
+            <h1>Texto vacio</h1>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default Formulari;
--- Practica3/src/components/LlistatTasques.jsx
+++ Practica3/src/components/LlistatTasques.jsx
@@ -1,28 +1,48 @@
 import React, { useState } from "react";
+import Formulari from "./Formulari";
 
-const LlistatTasques = () => {
+const LlistatTasques = props => {
     const [tasques, setTasques] = useState([]);
     // Afegir tasca
-    const afegirTasca = (tascaNova) => {
-        setTasques([...tasques, tascaNova]);
+    const afegirTasca = tascaNova => {
+        const tascActual = [...tasques, tascaNova]
+        setTasques(tascActual);
     };
     // Eliminar tasca
     const eliminarTasca = (id) => {
-        const tareasRestantes = tasques.filter((tasca, index) => index !== id);
-        setTasques(tareasRestantes);
+        const tasquesRestants = tasques.filter((tasca, index) => index !== id);
+        setTasques(tasquesRestants);
     };
     // Completar tasca 
     const completarTasca = (id) => {
-        const TascActual = [...tasques];
-        TascActual[id].Completa = !TascActual[id].Completa;
+        const TascActual = tasques.map((tasques, index) => {
+            if (index === id) {
+                return { ...tasques, completarTasca: !tasques.completada };
+            }
+            return tasques;
+        });
         setTasques(TascActual);
     };
 
-    return(
+    return (
         <div>
-            <h1>Mis tareas</h1>
+          <h1>Mis tareas</h1>
+          <Formulari funcAfegirTasca={afegirTasca} />
+          <ul>
+            {tasques.map((tasca, index) => (
+              <li key={index}>
+                {tasca.text}
+                <button onClick={() => completarTasca(index)}>
+                  Completar
+                </button>
+                <button onClick={() => eliminarTasca(index)}>
+                  Eliminar
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
-}
-
+      );
+    }
+    
 export default LlistatTasques;
